1d78be0c5d50af77df1709b2eac627137d39e250
Felix Apaza <felix.apaza19@gmail.com>
2021-11-02 06:33:00 -0500

    Fix types bug for tslint

---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+declare const defaultModule: any;
+
+declare module "react-native" {
+    export default SpInAppUpdates;
+    export const IAUUpdateKind: any;
+    export type StartUpdateOptions = any;
+}
